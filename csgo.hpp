a805c41068e70eaf6725cab788ae930882ed26db
SirCoDre <66209452+SirCoDre@users.noreply.github.com>
2020-08-27 09:00:33 +0300

    Add files via upload

---
+++ csgo.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <cstddef>
+
+// 2020-08-27 05:57:29.959289 UTC
+
+namespace hazedumper {
+namespace netvars {
+constexpr ::std::ptrdiff_t m_iCrosshairId = 0xB3E4;
+constexpr ::std::ptrdiff_t m_iGlowIndex = 0xA438;
+constexpr ::std::ptrdiff_t m_iTeamNum = 0xF4;
+} // namespace netvars
+namespace signatures {
+constexpr ::std::ptrdiff_t dwEntityList = 0x4D5554C;
+constexpr ::std::ptrdiff_t dwGetAllClasses = 0xD672BC;
+constexpr ::std::ptrdiff_t dwGlowObjectManager = 0x529D3F8;
+constexpr ::std::ptrdiff_t dwLocalPlayer = 0xD40C5C;
+} // namespace signatures
+} // namespace hazedumper
