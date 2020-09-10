6aac4ec733a02e0c54a592e64e9bfe31844f1827
SirCoDre <66209452+SirCoDre@users.noreply.github.com>
2020-09-10 12:20:38 +0300

    Update csgo.hpp

--- csgo.hpp
+++ csgo.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <cstddef>
 
-// 2020-08-28 20:18:30.626806700 UTC
+// 2020-09-10 09:19:30.486961700 UTC
 
 namespace hazedumper {
 namespace netvars {
@@ -10,9 +10,9 @@ constexpr ::std::ptrdiff_t m_iGlowIndex = 0xA438;
 constexpr ::std::ptrdiff_t m_iTeamNum = 0xF4;
 } // namespace netvars
 namespace signatures {
-constexpr ::std::ptrdiff_t dwEntityList = 0x4D5442C;
-constexpr ::std::ptrdiff_t dwGetAllClasses = 0xD6623C;
-constexpr ::std::ptrdiff_t dwGlowObjectManager = 0x529C208;
-constexpr ::std::ptrdiff_t dwLocalPlayer = 0xD3FC5C;
+constexpr ::std::ptrdiff_t dwEntityList = 0x4D4F1FC;
+constexpr ::std::ptrdiff_t dwGetAllClasses = 0xD6123C;
+constexpr ::std::ptrdiff_t dwGlowObjectManager = 0x5296FB0;
+constexpr ::std::ptrdiff_t dwLocalPlayer = 0xD3AC5C;
 } // namespace signatures
 } // namespace hazedumper
